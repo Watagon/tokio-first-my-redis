ea2873e3a2515f20235693ed645be4bcc088e507
watagon <coding.watahiki@gmail.com>
2023-04-02 10:31:47 +0900

    store values
    fix bug

--- src/main.rs
+++ src/main.rs
@@ -14,12 +14,30 @@ async fn main() {
 }
 
 async fn process(socket: TcpStream) {
+    use mini_redis::Command::{self, Get, Set};
+    use std::collections::HashMap;
+
+    let mut db = HashMap::new();
+
     let mut connection = Connection::new(socket);
 
-    if let Some(frame) = connection.read_frame().await.unwrap() {
-        println!("GOT: {frame:?}");
+    while let Some(frame) = connection.read_frame().await.unwrap() {
+        let response = match Command::from_frame(frame).unwrap() {
+            Set(cmd) => {
+                db.insert(cmd.key().to_owned(), cmd.value().to_vec());
+                Frame::Simple("OK".to_owned())
+            }
+            Get(cmd) => {
+                if let Some(value) = db.get(cmd.key()) {
+                    Frame::Bulk(value.clone().into())
+                }
+                else {
+                    Frame::Null
+                }
+            }
+            cmd => unimplemented!("{:?}", cmd),
+        };
 
-        let response = Frame::Error("unimplemented".to_string());
         connection.write_frame(&response).await.unwrap();
     }
 }
