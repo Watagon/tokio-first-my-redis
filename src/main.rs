8f4572bf95736cd2eec05e4a89ccf6d8573e6875
watagon <coding.watahiki@gmail.com>
2023-04-02 16:16:58 +0900

    create the channel

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,26 @@
 use bytes::Bytes;
+use tokio::sync::mpsc;
 
 #[derive(Debug)]
 enum Command {
     Get { key: String },
     Set { key: String, val: Bytes },
+}
+
+#[tokio::main]
+async fn main() {
+    let (tx, mut rx) = mpsc::channel(32);
+    let tx2 = tx.clone();
+
+    tokio::spawn(async move {
+        tx.send("sending from first handle").await;
+    });
+
+    tokio::spawn(async move {
+        tx2.send("sending from second handle").await;
+    });
+
+    while let Some(message) = rx.recv().await {
+        println!("GOT = {message}");
+    }
 }
